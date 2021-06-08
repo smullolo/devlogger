f22d42363e562762a6898ec0be4be5801ad26ff7
Isay Konter <isaykonter@gmail.com>
2021-06-08 09:35:29 +0200

    Added local storage

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -28,7 +28,12 @@ export class LogService {
   }
 
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    if(localStorage.getItem('logs') === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs')!);
+    }
+    return of(this.logs.sort((a, b) => {return b.date - a.date}));
   }
 
   setFormLog(log: Log) {
@@ -37,6 +42,9 @@ export class LogService {
 
   addLog(log: Log) {
     this.logs.unshift(log);
+    
+    //add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   updateLog(log: Log) {
@@ -46,6 +54,9 @@ export class LogService {
       }
     });
     this.logs.unshift(log);
+
+    //Update to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   deleteLog(log: Log) {
@@ -54,6 +65,9 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+
+    //Delete from local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   clearState() {
